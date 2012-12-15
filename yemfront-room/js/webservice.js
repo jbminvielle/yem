8d90f70575f91079a44f6cf30c1b1ddefaba3937
DrJaxx <durufe@gmail.com>
2012-12-15 19:25:34 +0100

    wut

--- yemfront-room/js/webservice.js
+++ yemfront-room/js/webservice.js
@@ -3,8 +3,8 @@ var Webservice = new Object();
 Webservice.get= function(value1, id1){
 							 
 		$.ajax({ 
-		   url: "bdd.php",
-		   data: { value : value1, id : id1 }
+		   url: "../../yemback/webservice.php",
+		   data: { value : value1, id : id1 },
 		   success: function(mov){
 		   		result = mov;
 		   }
@@ -16,8 +16,8 @@ Webservice.get= function(value1, id1){
 Webservice.kinect = function(){
 
 		$.ajax({ 
-		   url: "bdd.php",
-		   data: { humeur : humeur }
+		   url: "../../yemback/webservice.php",
+		   data: { humeur : humeur },
 		   success: function(mov){
 		   		result = mov;
 		   }
@@ -30,7 +30,7 @@ Webservice.kinect = function(){
 Webservice.nextQuestion = function(answer1){
 							 
 		$.ajax({ 
-		   url: "bdd.php",
+		   url: "../../yemback/webservice.php",
 		   data: { answer : answer1 }
 		   success: function(mov){
 		   		result = mov;
