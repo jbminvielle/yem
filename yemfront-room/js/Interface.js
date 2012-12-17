dc9ca56bce9eef32bc066860cf8f962810f810a8
Jean-Baptiste Minvielle <jean.baptiste.minvielle@gmail.com>
2012-12-18 00:02:56 +0100

    ajout de input

--- yemfront-room/js/Interface.js
+++ yemfront-room/js/Interface.js
@@ -4,7 +4,6 @@ if(typeof YEM == 'undefined') YEM = {};
 YEM.Interface = new Object();
 
 YEM.Interface.ShowTemplate = function(jsonobj, template){
-		alert("#template_"+template);
 		var templ = $("#template_"+template).html();
 
     	var html = Mustache.to_html(templ, jsonobj);
